917f552775b265ec648d5b740fc9056a68b314d5
niranju <35294168+niranju@users.noreply.github.com>
2018-01-17 16:17:53 +0530

    positivenegative.C
    positivenegative

---
+++ positivenegative.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int num;
+if(num>0)
+{
+printf("the number is positive");
+}
+if(num<0)
+{
+printf("the number is negative");
+}
+if(num==0)
+{
+printf("the number is zero");
+}
+return0;
+}
