22f32924f381c025c8bcd40660da996a1f286870
niranju <35294168+niranju@users.noreply.github.com>
2018-01-22 16:35:39 +0530

    nnames1.C
    print hello n times

---
+++ nnames1.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int count,n;
+    printf("enter n values");
+    scanf("%d",&n);
+    for(count=1;count<=n;count++)
+        {
+            printf("hello\n");
+        }
+        }
