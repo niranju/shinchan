63b823614581cb86206da3dad09999af0f86e744
niranju <35294168+niranju@users.noreply.github.com>
2018-02-09 16:01:24 +0530

    numdigits.C
    count numeric  digits

---
+++ numdigits.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+long long a;
+int count = 0;
+printf("Enter an integer: ");
+scanf("%lld", &a);
+while(a!= 0)
+{      
+a/= 10;
+++count;
+}
+printf("Number of digits: %d", count);
+}
