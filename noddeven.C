9cb582db25fdd1bd9c1815a475249fe12dcbfea8
niranju <35294168+niranju@users.noreply.github.com>
2018-02-12 16:01:22 +0530

    noddeven.C
    print odd or even numbers near to given n number

---
+++ noddeven.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,m;;
+printf("\n enter the number:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\n %d",a);
+}
+else
+{
+m=a-1;
+printf("\n %d",m);
+}
+return 0;
+}
