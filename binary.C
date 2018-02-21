5ee2265852eb1666fb93cc551da03590bf7798b7
niranju <35294168+niranju@users.noreply.github.com>
2018-02-21 16:08:13 +0530

    binary.C
    binary number or not

--- binary.C
+++ binary.C
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int i,count;
-int a[100];
+int b[100];
 printf("\n enter the number");
 for(i=0;i<=100;i++)
 {
-scanf("%d",&a[i]);
+scanf("%d",&b[i]);
 }
 for(i=0;i<=100;i++)
 {
-if(a[i]==0||a[i]==1)
+if(b[i]==0||b[i]==1)
 {
 count=1;
 }
