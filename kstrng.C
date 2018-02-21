21e5ea43d3d2e74de680e898a6347c852fb4715b
niranju <35294168+niranju@users.noreply.github.com>
2018-02-21 15:53:57 +0530

    kstrng.C
    for given k string print n characters

---
+++ kstrng.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[10];
+int i,k;
+printf("\n enter the string");
+scanf("%d",a);
+printf("\n enter the number");
+scanf("%d",&k);
+for(i=0;i<k;i++);
+{
+printf("%c",a[i]);
+}
+return 0;
+}
