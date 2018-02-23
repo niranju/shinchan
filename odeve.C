e366304536e857775d1c430edfc5199f06be3a62
niranju <35294168+niranju@users.noreply.github.com>
2018-02-23 16:06:37 +0530

    odeve.C
    odd or even number

---
+++ odeve.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("\n enter the two numbers:");
+scanf("%d,%d",&a,&b);
+c=a+b;
+if(sum%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+return 0;
+}
