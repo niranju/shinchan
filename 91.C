c380c99f46a51785533e46c4483c9012825c8b6d
niranju <35294168+niranju@users.noreply.github.com>
2018-03-05 15:37:53 +0530

    91.C
    program to calculate total surface area and volume

---
+++ 91.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float l,b,h,vol,totalarea;
+printf("\n enter the length breadth and height:");
+scanf("%f%f%f",&l,&b,&h);
+vol=l*b*h;
+tsa=2(l*b)+2(l*h)+2(h*b);
+printf("\n volume:%f",vol);
+printf("\n total suraface area:%f",totalarea);
+return 0;
+}
