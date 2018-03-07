51b67b1f294900a5b5dad5eb5fc0ffaaf989fdaf
niranju <35294168+niranju@users.noreply.github.com>
2018-03-07 16:33:24 +0530

    82.C

---
+++ 82.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+double l,b,total;
+printf("\n enter the length of the farm:");
+scanf("%lf",&l);
+printf("\n enter the breadth of the farm:");
+scanf("%lf",&b);
+total=l*b;
+printf("the  area is:%lf",total);
+return 0;
+}
