1a9e3c6b21ecb13457fd080d23f39c60509f1c89
niranju <35294168+niranju@users.noreply.github.com>
2018-03-09 15:47:21 +0530

    77.C
    to print the factors of given input

---
+++ 77.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int s,i;
+	printf("\n enter the number:");
+	scanf("%d",&s);
+	for(i=1;i<=n;i++)
+	{
+		if(s%i==0)
+		{
+			printf("\n %d",i);
+		}
+	}
+	return 0;
+}
