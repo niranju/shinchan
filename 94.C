6555ceea329762a65a3cce8a4c34353c11947842
niranju <35294168+niranju@users.noreply.github.com>
2018-03-19 16:04:36 +0530

    94.C
    to print k th number

---
+++ 94.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,a[10],i;
+ printf("\n enter the values and pos ");
+ scanf("%d %d",&a,&b);
+ for(i=0;i<a;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+ printf("\n the value in %d pos is:%d',k,a[k-1]);
+ getch();
+ return 0;
+ }
